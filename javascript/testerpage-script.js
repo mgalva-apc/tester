d398beeba6cd90887c3ae7277b6ebcb25c8ea2ed
Bea-Alva <116138375+Bea-Alva@users.noreply.github.com>
2023-03-01 18:13:38 +0800

    pushit

--- javascript/testerpage-script.js
+++ javascript/testerpage-script.js
@@ -1,25 +1,3 @@
-//pic rep
-var container = document.getElementById("picrep"); //LESSON 2.1 - VARIABLES//
-var slides = container.getElementsByClassName("picsforrep");
-
-function showSlidesAuto(s, i) {
-    s[i].style.opacity = "1"; //LESSON 7.1 - ARRAYS (array is 'slides' and is passed through s)//
-
-    if (i == 0) { //LESSON 9.4 - IF ELSE//
-        s[s.length-1].style.opacity = "0";
-    } else {
-        s[i-1].style.opacity = "0";
-    }
-
-    i++;
-    if (i >= s.length) { //LESSON 9.3 - COMPARISONS//
-        i = 0; //LESSON 5.2 - ASSINGMENTS//
-    }
-    setTimeout(function () { showSlidesAuto(s, i) }, 5000);
-}
-
-showSlidesAuto(slides, 0);
-
 //CONTENT 1 DIV
 var modalBtn = document.getElementById("berry-button1");
 var modal = document.getElementById("berry-modal");
@@ -44,3 +22,25 @@ function showSlides(n){
     vidSlides[index].style.opacity = "1"
     txtSlides[index].style.opacity = "1"
 }
+
+//pic rep
+var container = document.getElementById("picrep"); //LESSON 2.1 - VARIABLES//
+var slides = container.getElementsByClassName("picsforrep");
+
+function showSlidesAuto(s, i) {
+    s[i].style.opacity = "1"; //LESSON 7.1 - ARRAYS (array is 'slides' and is passed through s)//
+
+    if (i == 0) { //LESSON 9.4 - IF ELSE//
+        s[s.length-1].style.opacity = "0";
+    } else {
+        s[i-1].style.opacity = "0";
+    }
+
+    i++;
+    if (i >= s.length) { //LESSON 9.3 - COMPARISONS//
+        i = 0; //LESSON 5.2 - ASSINGMENTS//
+    }
+    setTimeout(function () { showSlidesAuto(s, i) }, 5000);
+}
+
+showSlidesAuto(slides, 0);
