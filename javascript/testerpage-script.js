d190ddca80a4e9a40da15dba8d416fb85248b10e
Bea-Alva <116138375+Bea-Alva@users.noreply.github.com>
2023-03-01 22:09:00 +0800

    i accomplish stuff

--- javascript/testerpage-script.js
+++ javascript/testerpage-script.js
@@ -43,4 +43,52 @@ function showSlidesAuto(s, i) {
     setTimeout(function () { showSlidesAuto(s, i) }, 5000);
 }
 
+//CONTENT 2 DIV
+var modalBtn2 = document.getElementById("berry-button2");
+var modal2 = document.getElementById("berry-modal2");
+modalBtn2.addEventListener('click', function() { modal2.showModal() } );
+var cancelBtn2 = document.getElementById("berry-cancel2");
+cancelBtn2.addEventListener('click', function() { modal2.close()} );
+var vidSlides2 = document.getElementById("berry-slidebox2").getElementsByClassName("berry-vids2");
+var txtSlides2 = document.getElementById("berry-slidebar2").getElementsByClassName("slidebar-content2");
+var index2 = 0;
+function showSlides2(n2){
+    index2 += n2;
+    for (let i=0; i<vidSlides2.length; ++i) { 
+        vidSlides2[i].style.opacity = "0";
+        txtSlides2[i].style.opacity = "0";
+    }
+    if (index2 >= vidSlides2.length) {
+        index2 = 0;
+    }
+    if (index2 < 0) {
+        index2=vidSlides2.length-1;
+    }
+    vidSlides2[index2].style.opacity = "1"
+    txtSlides2[index2].style.opacity = "1"
+}
+
+//pic rep
+var container2 = document.getElementById("picrep2");
+var slides2 = container2.getElementsByClassName("picsforrep2");
+
+function showSlidesAuto2(s, i) {
+    s[i].style.opacity = "1"; 
+
+    if (i == 0) { 
+        s[s.length-1].style.opacity = "0";
+    } else {
+        s[i-1].style.opacity = "0";
+    }
+
+    i++;
+    if (i >= s.length) { 
+        i = 0; 
+    }
+    setTimeout(function () { showSlidesAuto2(s, i) }, 5000);
+}
+
 showSlidesAuto(slides, 0);
+showSlides(0);
+showSlidesAuto2(slides, 0);
+showSlides2(0);
